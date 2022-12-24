baba58823268c3f8fbd54dca96740964943e095f
GE-2000 <77683018+GE-2000@users.noreply.github.com>
2022-12-24 19:39:43 +0200

    Main

---
+++ backend/models/candidatesModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const candidateSchema = new Schema({
+    name:{
+        type : String,
+        required:true
+    },
+
+    id:{
+        type:Number,
+        required:true
+    },
+
+    load:{
+        type:Number,
+        required :true
+    }
+
+
+},
+{timestamps:true})
+
+module.exports = mongoose.model('Candidate',candidateSchema )
